f28508e496b3de242ac3d246aaf915a065a79ddf
Dron <dron2203@gmail.com>
2023-05-27 16:58:54 +0300

    Сделал программу вычисления квадратного уравнения

--- kv_ur.py
+++ kv_ur.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 bad_data = True
 while bad_data == True:
     try:
@@ -7,3 +9,14 @@ while bad_data == True:
         bad_data = False
     except:
         print('Ведены неверные данные')
+D = b * b - (4 * a * c)
+print(f'Дискриминант равен {D}')
+if D < 0:
+    print('уравнение не имеет корней')
+elif D == 0:
+    x1 = (-b) / 2 * a
+    print(f'Уравнение имеет один корень, X1={x1}')
+else:
+    x1 = ((-b) + sqrt(D)) / (2 * a)
+    x2 = ((-b) - sqrt(D)) / (2 * a)
+    print(f'Корни уравнения X1={x1},X2={x2}')
